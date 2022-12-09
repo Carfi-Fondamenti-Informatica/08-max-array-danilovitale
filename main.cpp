18ac6300ff3ca608c822e4d11a7520a4528b27b9
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-12-09 22:16:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main()
+{
+    int n=0; 
+    float h=0;
+    cin>>n;
+    float max[n]={0};
+    for(int i=0;i<n;i++)
+    {
+        cin>>h;
+        max[i]=h;
+    }
+    
+    cout<<massimo(max, n);
 }
