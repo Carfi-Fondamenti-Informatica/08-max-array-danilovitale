70e058f978232ac99db5fb7e33337abf581a444a
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-12-09 22:16:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include "lib.h"
 
+float massimo(const float max[], int &n){
+    int c=0, s=0, y=1;
+ for (int i =0;i<n;i++)
+       {
+         if(max[c]>max[y])
+         {
+             y++;
+             s=max[c];
+         }
+         else {
+             s=max[y];
+            c=y;
+            y++;
+            
+         }
+       }
+    return s;
+}
